ac4b639a20b4342192acc93eaedec527a1af73ef
discovermohit <mohit.mern@aesl.in>
2022-06-28 20:03:00 +0530

    updated

--- eks/main.tf
+++ eks/main.tf
@@ -227,28 +227,28 @@ resource "aws_eks_node_group" "main" {
   ]
 }
 
-# data "template_file" "kubeconfig" {
-#   template = file("${path.module}/templates/kubeconfig.tpl")
+data "template_file" "kubeconfig" {
+  template = file("${path.module}/templates/kubeconfig.tpl")
 
-#   vars = {
-#     kubeconfig_name           = "eks_${aws_eks_cluster.main.name}"
-#     clustername               = aws_eks_cluster.main.name
-#     endpoint                  = data.aws_eks_cluster.cluster.endpoint
-#     cluster_auth_base64       = data.aws_eks_cluster.cluster.certificate_authority[0].data
-#   }
-# }
+  vars = {
+    kubeconfig_name           = "eks_${aws_eks_cluster.main.name}"
+    clustername               = aws_eks_cluster.main.name
+    endpoint                  = data.aws_eks_cluster.cluster.endpoint
+    cluster_auth_base64       = data.aws_eks_cluster.cluster.certificate_authority[0].data
+  }
+}
 
-# resource "local_file" "kubeconfig" {
-#   content  = data.template_file.kubeconfig.rendered
-#   filename = pathexpand("${var.kubeconfig_path}/config")
-# }
+resource "local_file" "kubeconfig" {
+  content  = data.template_file.kubeconfig.rendered
+  filename = pathexpand("${var.kubeconfig_path}/config")
+}
 
-# output "kubectl_config" {
-#   description = "Path to new kubectl config file"
-#   value       = pathexpand("${var.kubeconfig_path}/config")
-# }
+output "kubectl_config" {
+  description = "Path to new kubectl config file"
+  value       = pathexpand("${var.kubeconfig_path}/config")
+}
 
-# output "cluster_id" {
-#   description = "ID of the created cluster"
-#   value       = aws_eks_cluster.main.id
-# }
+output "cluster_id" {
+  description = "ID of the created cluster"
+  value       = aws_eks_cluster.main.id
+}
